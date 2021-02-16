2c8a7e9ab6fc5aa4e7cab7e7ae344b1014db930f
raouanedev <raouanedev@gmail..com>
2021-02-16 01:07:51 +0100

    add driver

--- config/plugins.js
+++ config/plugins.js
@@ -1,16 +1,20 @@
-module . exports  =  ( {  env  } )  =>  ( { 
+
+// production script
+// ******************************
+
+// module . exports  =  ( {  env  } )  =>  ( { 
     
-    upload : { 
-      provider : 'cloudinary' , 
-      providerOptions : { 
-        cloud_name : env ( 'CLOUDINARY_NAME' ) , 
-        api_key : env ( 'CLOUDINARY_KEY' ) , 
-        api_secret : env ( 'CLOUDINARY_SECRET' ) , 
-      } , 
-      actionOptions : { 
-        upload: { } , 
-        supprimer : { } 
-      } 
-    } , 
+//     upload : { 
+//       provider : 'cloudinary' , 
+//       providerOptions : { 
+//         cloud_name : env ( 'CLOUDINARY_NAME' ) , 
+//         api_key : env ( 'CLOUDINARY_KEY' ) , 
+//         api_secret : env ( 'CLOUDINARY_SECRET' ) , 
+//       } , 
+//       actionOptions : { 
+//         upload: { } , 
+//         supprimer : { } 
+//       } 
+//     } , 
      
-  } ) ;
+//   } ) ;
