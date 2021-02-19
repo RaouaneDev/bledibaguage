ac8cbc6f112ad2640ca02af34a1d0bdf71f7a77f
raouanedev <raouanedev@gmail.com>
2021-02-19 01:55:19 +0100

    add required fiel

--- config/database.js
+++ config/database.js
@@ -3,48 +3,48 @@
     // ******************************
 
 
+// module.exports = ({ env }) => ({
+//   defaultConnection: 'default',
+//   connections: {
+//     default: {
+//       connector: 'mongoose',
+//       settings: {
+//         host: env('DATABASE_HOST'),
+//         srv: env.bool('DATABASE_SRV', true),
+//         port: env.int('DATABASE_PORT', 27017),
+//         database: env('DATABASE_NAME'),
+//         username: env('DATABASE_USERNAME'),
+//         password: env('DATABASE_PASSWORD'),
+//       },
+//       options: {
+
+//         authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
+//         ssl: env.bool('DATABASE_SSL', true),
+//       },
+//     },
+//   },
+// });
+
+//  // for local developemnt script
+//     // ******************************
+
 module.exports = ({ env }) => ({
   defaultConnection: 'default',
   connections: {
     default: {
       connector: 'mongoose',
       settings: {
-        host: env('DATABASE_HOST'),
+        host: env('DATABASE_HOST', '@cluster0.39h7c.mongodb.net'),
         srv: env.bool('DATABASE_SRV', true),
         port: env.int('DATABASE_PORT', 27017),
-        database: env('DATABASE_NAME'),
-        username: env('DATABASE_USERNAME'),
-        password: env('DATABASE_PASSWORD'),
+        database: env('DATABASE_NAME', 'bledibaguage'),
+        username: env('DATABASE_USERNAME', 'Raouane'),
+        password: env('DATABASE_PASSWORD', 'Papa1930'),
       },
       options: {
-
         authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
         ssl: env.bool('DATABASE_SSL', true),
       },
     },
   },
 });
-
-//  // for local developemnt script
-//     // ******************************
-
-// // module.exports = ({ env }) => ({
-// //   defaultConnection: 'default',
-// //   connections: {
-// //     default: {
-// //       connector: 'mongoose',
-// //       settings: {
-// //         host: env('DATABASE_HOST', '@cluster0.39h7c.mongodb.net'),
-// //         srv: env.bool('DATABASE_SRV', true),
-// //         port: env.int('DATABASE_PORT', 27017),
-// //         database: env('DATABASE_NAME', 'bledibaguage'),
-// //         username: env('DATABASE_USERNAME', 'Raouane'),
-// //         password: env('DATABASE_PASSWORD', 'Papa1930'),
-// //       },
-// //       options: {
-// //         authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
-// //         ssl: env.bool('DATABASE_SSL', true),
-// //       },
-// //     },
-// //   },
-// // });
